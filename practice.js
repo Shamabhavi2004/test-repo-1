e0153234b362a178eeaad3030bcba4486dfc790b
Shamabhavi2004 <lalithashambhavibojjapu@gmail.com>
2026-01-30 13:12:20 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,12 @@
+var a="playing";
+console.log(a)
+
+var a =10;
+console.log(a)
+//------------------------------
+
+let ramu="sings well"
+console.log(ramu)
+
+const rupa="poor in studies"
+console.log(rupa)
